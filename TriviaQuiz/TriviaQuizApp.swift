6daf8c073dcd7fa55e93c227560fd7a3f7ae14ae
Sarah <sarahlidberg@live.se>
2022-02-23 13:48:26 +0100

    Initial Commit

---
+++ TriviaQuiz/TriviaQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaQuizApp.swift
+//  TriviaQuiz
+//
+//  Created by Sarah Lidberg on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
