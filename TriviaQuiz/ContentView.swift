6626eba1368a3d304181909670624d35d411d29e
Andreas Jonasson <officialskum@gmail.com>
2022-02-24 15:11:32 +0100

    swagStashen 8^)

--- TriviaQuiz/ContentView.swift
+++ TriviaQuiz/ContentView.swift
@@ -10,32 +10,51 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        
-        NavigationView{
-            
+
+        NavigationView {
+
             ZStack {
-            
+
                 LinearGradient(colors: [.red, .yellow, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
                     .ignoresSafeArea()
-                
-                ZStack{
-                                    
-                Circle()
-                    //.fill(.black)
-                        .fill(LinearGradient(colors: [.red, .white], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .padding()
-                    
-                NavigationLink(destination: TriviaView()) {
-                    
-                    Text("Enter game")
-                        .foregroundColor(.white)
-                        .font(.title)
+
+                VStack {
+                    HStack {
+
+                    Text("TriviaQuiz")
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                        .shadow(color: .black, radius: 2)
+                        .padding()
+                        Spacer()
+                    }
+                    NavigationLink(destination: TriviaView()) {
+                        ZStack {
+                            Circle()
+                                .fill(LinearGradient(
+                                    colors:
+                                        [.red, .white],
+                                    startPoint:
+                                            .topLeading,
+                                    endPoint:
+                                            .bottomTrailing))
+                                .shadow(color: .black, radius: 5)
+                                .padding()
+
+                            Button {
+
+                            } label: {
+                                Text("Enter game")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                            }
+                        }.offset(y: -60)
+                    }
                 }
-                }
-                .offset(y: -60)
-                .navigationTitle("TriviaQuiz")
-                
             }
+            //.offset(y: -60)
+            .navigationBarHidden(true)
+            //.navigationTitle("TriviaQuiz").navigationViewStyle(NavigationViewStyle.)
         }
     }
 }
