fe37cfeb43855ad22932c5d3160471ae85936ee6
Sarah <sarahlidberg@live.se>
2022-03-09 16:27:08 +0100

    sarah did something, kommer ej ihåg dock lol

--- TriviaQuiz/Component/TriviaButton.swift
+++ TriviaQuiz/Component/TriviaButton.swift
@@ -13,10 +13,10 @@ struct TriviaButton: View {
     
     var text: String
     var background: Color?
-
+    
     @EnvironmentObject var triviaManager : TriviaManager
     @State private var didTap: Bool = false
-
+    
     var body: some View {
         
         Button(action: {
@@ -27,7 +27,7 @@ struct TriviaButton: View {
                 triviaManager.score += 1
                 print(triviaManager.score)
             }
-           // triviaManager.nextQuestion(backwards: triviaManager.backwards)
+            // triviaManager.nextQuestion(backwards: triviaManager.backwards)
             
             // self.opacity(configuration.isPressed ? 0.7 : 1)
         }) {
@@ -36,8 +36,8 @@ struct TriviaButton: View {
                 .frame(width: 280, height: 50)
                 .background(.blue.opacity(0.5))
                 .cornerRadius(10)
-               // .shadow(radius: 5)
-               // .shadow(color: triviaManager.isGameEnded ? (.pink) : .gray, radius: 5, x: 0.5, y: 0.5)
+            // .shadow(radius: 5)
+            // .shadow(color: triviaManager.isGameEnded ? (.pink) : .gray, radius: 5, x: 0.5, y: 0.5)
         }
         .buttonStyle(MyButtonStyle(didTap: self.didTap))
     }
@@ -46,27 +46,31 @@ struct TriviaButton: View {
 
 struct MyButtonStyle: ButtonStyle {
     @EnvironmentObject var triviaManager : TriviaManager
-
+    
     let blueColorArray = [Color.blue, Color.white]
-
+    
     var didTap : Bool
-
-
+    var isButtonPressed = false
+    
+    
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .padding()
-
+            .padding(5)
+        
             .background(triviaManager.isGameEnded ? (LinearGradient(colors: triviaManager.isColorMode ? blueColorArray : [.red, .white],
                                                                     startPoint: .topLeading,
                                                                     endPoint: .bottomTrailing))
                         : (LinearGradient(colors: triviaManager.isColorMode ? blueColorArray : [.red, .yellow, .purple],
                                           startPoint: .topLeading,
                                           endPoint: .bottomTrailing)), in: Capsule())
-            .opacity(configuration.isPressed ? 0.5 : 1)
-
+        //            .opacity(configuration.isPressed ? 0.5 : 1)
+        
             .background(triviaManager.isGameEnded ? (LinearGradient(colors: [.red, .white], startPoint: .topLeading, endPoint: .bottomTrailing)) : (LinearGradient(colors: [.red, .yellow, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)), in: Capsule())
-           // .opacity(configuration.isPressed ? 0.5 : 1)
-            .opacity(self.didTap ? 0.2 : 1)
-
+//         .opacity(configuration.isPressed ? 0.5 : 1)
+                    .opacity(self.didTap ? 0.2 : 1)
+        
     }
+    
 }
+
+
--- TriviaQuiz/Views/TriviaView.swift
+++ TriviaQuiz/Views/TriviaView.swift
@@ -13,6 +13,7 @@ struct TriviaView : View {
     @EnvironmentObject var triviaManager : TriviaManager
     let blueColorArray = [Color.blue, Color.white]
     var currentGame = 0
+    var isButtonPressed = false
     
     var body: some View {
         
@@ -21,54 +22,85 @@ struct TriviaView : View {
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .ignoresSafeArea()
-//            AnimatedBackground().edgesIgnoringSafeArea(.all)
-//                .blur(radius: 50)
-
+            //            AnimatedBackground().edgesIgnoringSafeArea(.all)
+            //                .blur(radius: 50)
+            
             VStack{
-                Spacer()
+               
                 ZStack {
-                    Rectangle()
+                    Circle()
                         .fill(LinearGradient(colors: triviaManager.isColorMode ? blueColorArray : [.blue, .red],
                                              startPoint: .topLeading,
                                              endPoint: .bottomTrailing))
                         .frame(height: 280, alignment: .center)
-                        .shadow(color: .white, radius: 5)
-                        .padding()
+                        .shadow(color: .white, radius: 10)
+                        .padding(20)
+                        
                     Text(triviaManager.questionToDisplay)
                         .fontWeight(.bold)
                         .font(.headline)
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
-                        .padding(50)
+                        .padding(60)
+                        
                 }
-                Spacer()
-                VStack {
+                .offset(y: -10)
+                
+//                VStack {
                     ForEach(triviaManager.allAnswersDecoded, id: \.self) {answer in
                         TriviaButton(text: answer)
                     }
-                }
-                Spacer()
-                HStack {
-                    Button(action: {
-                        if triviaManager.index >= 1 {
-                            triviaManager.index -= 1
-                        } else {
-                            triviaManager.index = 0
-                        }
-                        triviaManager.nextQuestion()
-                    }, label: {Text("Back")})
-                    Spacer()
-                    Button(action: {
-                        triviaManager.index += 1
-                        triviaManager.nextQuestion()
-                        
-                    }, label: {Text("Next")})
-                }
+//                }
+             
+//                .offset(y: -30)
                 NavigationLink(destination: ScoreView(), isActive: $triviaManager.isGameEnded) {EmptyView()}
-                Spacer()
+              Spacer()
             }
+            
+            VStack{
+                Spacer()
+            
+            HStack {
+                Button(action: {
+                    if triviaManager.index >= 1 {
+                        triviaManager.index -= 1
+                    } else {
+                        triviaManager.index = 0
+                    }
+                    triviaManager.nextQuestion()
+                }, label: {LinearGradient(gradient: Gradient(colors: [.pink, .blue]),
+                                          startPoint: .top,
+                                          endPoint: .bottom)
+                        .frame(width: 50, height: 50)
+                        .mask(Image(systemName: "arrow.left.circle"))
+                        .font(.system(size: 50.0))
+                        .padding(.leading, 20)
+                    
+//                        .offset(x:40)
+                })
+                Spacer()
+                Button(action: {
+                    triviaManager.index += 1
+                    triviaManager.nextQuestion()
+                    
+                }, label: {
+                    LinearGradient(gradient: Gradient(colors: [.pink, .blue]),
+                                   startPoint: .top,
+                                   endPoint: .bottom)
+                        .frame(width: 50, height: 50)
+                        .mask(Image(systemName: "arrow.right.circle"))
+                        .font(.system(size: 50.0))
+                        .padding(.trailing, 20)
+                        
+//                        .offset(x:40)
+                })
+            }
+               
+            }
+            
+            
         }
-        .navigationTitle("TriviaQuiz")
+//                .navigationTitle("TriviaQuiz")
         .navigationBarItems(trailing: QuestionIndex())
     }
 }
